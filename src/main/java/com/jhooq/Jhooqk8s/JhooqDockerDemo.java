69ad114090590cd007452320e73adb8733407374
Srini555 <34304857+Srini555@users.noreply.github.com>
2021-02-06 23:57:11 +0530

    Update JhooqDockerDemo.java
    Added comment

--- src/main/java/com/jhooq/Jhooqk8s/JhooqDockerDemo.java
+++ src/main/java/com/jhooq/Jhooqk8s/JhooqDockerDemo.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JhooqDockerDemo {
+    //Srinivas
 
     public static void main(String[] args) {
         System.out.println("JhooqDockerDemo");
